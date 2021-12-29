df729f86bf066087dcfaaae5546dd454f3ac1443
anubha0216 <85566996+anubha0216@users.noreply.github.com>
2021-12-29 10:16:01 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "CHAIR";
+const accessCode2 = "WASHING MACHINE";
+const accessCode3 = "PILLOW";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R A I C H", 100,50) 
+    fill("yellow")
+    text("Hint:I have four legs but no feet. When you get tired, have a seat.", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("W S I G A H N A H N M C I E", 600,150)  
+    fill("yellow")
+    text("Hint: Things go inside me when dirty but come out clean. You will find me standing sturdy.", 400,170)
+
+    fill("white")
+    textSize(15)
+    text("L L W P O I", 100,250) 
+    fill("yellow")
+    text("Hint: Turn out the light and rest your head on me tonight.", 100,270)
+
+}
